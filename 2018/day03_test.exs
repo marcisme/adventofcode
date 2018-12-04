69a5ada80d5e329ddfc3090091bfc47291eefd3d
Marc Schwieterman <marc@marcisme.com>
2018-12-03 20:34:05 -0800

    Complete day03part2

--- 2018/day03_test.exs
+++ 2018/day03_test.exs
@@ -1,17 +1,55 @@
 defmodule Day03 do
-  def overlap(claims) do
-    Enum.reduce(claims, %{}, fn claim, counts ->
-      fields = Regex.named_captures(~r/#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<w>\d+)x(?<h>\d+)/, claim)
+  defmodule Claim do
+    defstruct [:id, :x, :y, :w, :h]
+
+    def new(s) do
+      fields = Regex.named_captures(~r/#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<w>\d+)x(?<h>\d+)/, s)
+      id = fields["id"] |> String.to_integer
       x = fields["x"] |> String.to_integer
       y = fields["y"] |> String.to_integer
       w = fields["w"] |> String.to_integer
       h = fields["h"] |> String.to_integer
-      Enum.reduce(x..x+w-1, counts, fn i, counts ->
-        Enum.reduce(y..y+h-1, counts, fn j, counts ->
+      %Claim{id: id, x: x, y: y, w: w, h: h}
+    end
+  end
+
+  def overlap(claims) do
+    overlap_map(claims) |> Enum.count(fn {_k, v} -> v > 1 end)
+  end
+
+  defp overlap_map(claims) do
+    Enum.reduce(claims, %{}, fn claim_string, counts ->
+      claim = Claim.new(claim_string)
+      Enum.reduce(claim.x..claim.x+claim.w-1, counts, fn i, counts ->
+        Enum.reduce(claim.y..claim.y+claim.h-1, counts, fn j, counts ->
           Map.update(counts, {i, j}, 1, &(&1 + 1))
         end)
       end)
-    end) |> Enum.count(fn {_k, v} -> v > 1 end)
+    end)
+  end
+
+  def non_overlapping_id(claims) do
+    overlap_map = overlap_map(claims)
+    Enum.reduce_while(claims, nil, fn claim_string, _ ->
+      claim = Claim.new(claim_string)
+      outer_result = Enum.reduce_while(claim.x..claim.x+claim.w-1, nil, fn i, _ ->
+        inner_result = Enum.reduce_while(claim.y..claim.y+claim.h-1, nil, fn j, _ ->
+          if overlap_map[{i, j}] == 1 do
+            {:cont, claim.id}
+          else
+            {:halt, nil}
+          end
+        end)
+        case inner_result do
+          nil -> {:halt, nil}
+          id -> {:cont, id}
+        end
+      end)
+      case outer_result do
+        nil -> {:cont, nil}
+        id -> {:halt, id}
+      end
+    end)
   end
 end
 
@@ -45,5 +83,17 @@ defmodule Day03Test do
   # 1,6 2,6 3,6 4,6
 
   describe "part 2" do
+    test "test claims" do
+      assert Day03.non_overlapping_id([
+        "#1 @ 1,3: 4x4",
+        "#2 @ 3,1: 4x4",
+        "#3 @ 5,5: 2x2"
+      ]) == 3
+    end
+
+    test "answer" do
+      answer = parse(@input_file) |> Day03.non_overlapping_id
+      assert answer == 1260
+    end
   end
 end
